3f0b7579442d5999d74ef0cf027d7b97c17b5a29
thomasdriscoll <thomasdriscoll98@gmail.com>
2021-08-30 07:14:28 -0500

    working POST TaskController tests

--- src/test/java/com/thomasdriscoll/productivityapi/controller/TaskControllerTest.java
+++ src/test/java/com/thomasdriscoll/productivityapi/controller/TaskControllerTest.java
@@ -7,6 +7,7 @@ import com.thomasdriscoll.productivityapi.lib.enums.StatusType;
 import com.thomasdriscoll.productivityapi.lib.enums.TypeTask;
 import com.thomasdriscoll.productivityapi.lib.exceptions.DriscollException;
 import com.thomasdriscoll.productivityapi.lib.exceptions.TaskExceptions;
+import com.thomasdriscoll.productivityapi.lib.exceptions.UserExceptions;
 import com.thomasdriscoll.productivityapi.lib.models.TaskDto;
 import com.thomasdriscoll.productivityapi.lib.models.TaskRequest;
 import com.thomasdriscoll.productivityapi.lib.responses.DriscollResponse;
@@ -27,6 +28,7 @@ import org.springframework.test.web.servlet.MvcResult;
 
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -101,14 +103,14 @@ class TaskControllerTest {
     @DisplayName("Create Task tests")
     class CreateTaskDtoRequestTests {
         ObjectMapper mapper = new ObjectMapper();
-        String request = mapper.writeValueAsString(TASK_REQUEST);
+
 
         CreateTaskDtoRequestTests() throws JsonProcessingException {
         }
 
         @Test
         public void givenTask_whenCreateTask_thenReturn201() throws Exception {
-
+            String request = mapper.writeValueAsString(TASK_REQUEST);
             String expected = mapper.writeValueAsString(new DriscollResponse<>(HttpStatus.CREATED.value(), TASK_DTO));
 
             when(taskService.createTask(USER_ID, TASK_REQUEST)).thenReturn(TASK_DTO);
@@ -124,25 +126,27 @@ class TaskControllerTest {
 
         @Test
         public void givenTask_whenInvalidUser_return404() throws Exception {
-//            DriscollException exception = new DriscollException(UserExceptions.USER_NOT_FOUND.getStatus(), UserExceptions.USER_NOT_FOUND.getMessage());
-//            String expected = mapper.writeValueAsString(new DriscollResponse<>(exception.getStatus().value(), exception.getMessage()));
-//
-//            //Mock what needs to be mocked
-//            when(userService.validateUser(BAD_USER)).thenThrow(exception);
-//
-//            //Do test
-//            MvcResult result = mockMvc.perform(post(String.format("/users/%s/tasks", USER_ID))
-//                    .contentType(MediaType.APPLICATION_JSON)
-//                    .content(request))
-//                    .andExpect(status().isNotFound())
-//                    .andReturn();
-//
-//            //Assert if test worked
-//            assertEquals(expected, result.getResponse().getContentAsString());
+            String request = mapper.writeValueAsString(TASK_REQUEST);
+            DriscollException exception = new DriscollException(UserExceptions.USER_NOT_FOUND.getStatus(), UserExceptions.USER_NOT_FOUND.getMessage());
+            String expected = mapper.writeValueAsString(new DriscollResponse<>(exception.getStatus().value(), exception.getMessage()));
+
+            //Mock what needs to be mocked
+            doThrow(exception).when(userService).validateUser(BAD_USER);
+
+            //Do test
+            MvcResult result = mockMvc.perform(post(String.format("/users/%s/tasks", BAD_USER))
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(request))
+                    .andExpect(status().isNotFound())
+                    .andReturn();
+
+            //Assert if test worked
+            assertEquals(expected, result.getResponse().getContentAsString());
         }
 
         @Test
         public void givenTask_whenInvalidPriority_return400() throws Exception {
+            String request = mapper.writeValueAsString(TASK_REQUEST_INVALID_PRIORITY);
             DriscollException exception = new DriscollException(TaskExceptions.INVALID_TASK_PRIORITY.getStatus(), TaskExceptions.INVALID_TASK_PRIORITY.getMessage());
             String expected = mapper.writeValueAsString(new DriscollResponse<>(exception.getStatus().value(), exception.getMessage()));
 
@@ -159,6 +163,7 @@ class TaskControllerTest {
 
         @Test
         public void givenTask_whenInvalidType_return400() throws Exception {
+            String request = mapper.writeValueAsString(TASK_REQUEST_INVALID_TYPE);
             DriscollException exception = new DriscollException(TaskExceptions.INVALID_TASK_TYPE.getStatus(), TaskExceptions.INVALID_TASK_TYPE.getMessage());
             String expected = mapper.writeValueAsString(new DriscollResponse<>(exception.getStatus().value(), exception.getMessage()));
 
@@ -175,6 +180,7 @@ class TaskControllerTest {
 
         @Test
         public void givenTask_whenInvalidStatus_return400() throws Exception {
+            String request = mapper.writeValueAsString(TASK_REQUEST_INVALID_STATUS);
             DriscollException exception = new DriscollException(TaskExceptions.INVALID_STATUS.getStatus(), TaskExceptions.INVALID_STATUS.getMessage());
             String expected = mapper.writeValueAsString(new DriscollResponse<>(exception.getStatus().value(), exception.getMessage()));
 
